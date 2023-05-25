8cd5418cda99600aff2f8cd314f4e466efd9d822
Nachyjdv <97794394+Nachyjdv@users.noreply.github.com>
2023-05-25 11:43:06 +0530

    Add files via upload

---
+++ build/precache-manifest.d27b106053931ae22c209336f19ba668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df018772f37bc155886c91924dfbc75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba364066c678f105415b",
+    "url": "./static/css/main.d8ff4c56.chunk.css"
+  },
+  {
+    "revision": "fa5b9a970df9c08c86d5",
+    "url": "./static/js/2.43a3d1eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.43a3d1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba364066c678f105415b",
+    "url": "./static/js/main.5da9963a.chunk.js"
+  },
+  {
+    "revision": "7b8ec6ea06e8c6a3eba8",
+    "url": "./static/js/runtime-main.024a0a5d.js"
+  },
+  {
+    "revision": "49caa1e8219db034d836b45389f97c22",
+    "url": "./static/media/loading.49caa1e8.svg"
+  }
+]);
